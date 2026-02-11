353003bef87bb50029b8ec1c05777102288982ca
KashypaKristipati <159120622+KashypaKristipati@users.noreply.github.com>
2026-02-11 23:39:56 +0530

    Solve: 838. Problem 838 (Medium)

---
+++ Medium/838_problem_838_e5bfee.py
@@ -0,0 +1,14 @@
+# 838. LeetCode Problem
+# LeetCode Link: https://leetcode.com/problems/
+
+class Solution:
+    def solve(self):
+        """
+        LeetCode Problem #838
+        
+        AI generation temporarily unavailable.
+        Visit LeetCode to solve this problem manually.
+        """
+        pass
+
+# Solved: 2026-02-11
